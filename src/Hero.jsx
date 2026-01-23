1a87f3093a036962bf7bcd99168b4976f9142461
Saikrishna-1918 <krishna.gangadhara@peopletech.com>
2026-01-23 12:11:34 +0530

    Update hero content

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,10 +1,11 @@
 const Hero = ({ blok }) => {
-  return (
-    <section>
-      <h1>{blok.title}</h1>
-      <p>{blok.description}</p>
-    </section>
-  );
+    return (
+        <section>
+            <h1>Hello from Storyblok 🚀</h1>
+            <h1>{blok.title}</h1>
+            <p>{blok.description}</p>
+        </section>
+    );
 };
 
 export default Hero;
