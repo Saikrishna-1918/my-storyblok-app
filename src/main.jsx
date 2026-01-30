fb44c40e939eb0ffc033ca4cad4f7a29eaaded11
Saikrishna-1918 <krishna.gangadhara@peopletech.com>
2026-01-30 14:45:12 +0530

    file_name_change

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./storyblok";
+import "./storyblok";   // 👈 this initializes Storyblok
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
